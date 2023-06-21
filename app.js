66f3bf242bb89680082687525162a3d47420d188
Bruno Vicente <brunovicente.dev@gmail.com>
2023-06-21 09:57:09 -0300

    Added show route

--- app.js
+++ app.js
@@ -20,9 +20,15 @@ app.set("views", path.join(__dirname, "views"));
 app.get("/", (req, res) => {
   res.render("home");
 });
+
 app.get("/campgrounds", async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render("campgrounds/index", { campgrounds });
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", { campground });
 });
 
 app.listen(3000, () => {
