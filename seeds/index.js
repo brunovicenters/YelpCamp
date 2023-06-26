a20e7fc6e633acc46d7e4e1c4e080af8498e03d6
Bruno Vicente <brunovicente.dev@gmail.com>
2023-06-26 11:03:00 -0300

    Addedd random imgs and descriptions

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const axios = require("axios");
 const cities = require("./cities");
 const { places, descriptors } = require("./seedHelpers");
 const Campground = require("../models/campground");
@@ -14,13 +15,32 @@ db.once("open", () => console.log("Database connected"));
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
+async function seedImg() {
+  try {
+    const resp = await axios.get("https://api.unsplash.com/photos/random", {
+      params: {
+        client_id: "XTRe8ys1xW0Gd3s7kTPoGnOmPalgAB6-J9-asX3LHzU",
+        collections: 1114848,
+      },
+    });
+    return resp.data.urls.small;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 25; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: await seedImg(),
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis magnam sequi quis, animi corrupti, velit earum voluptatibus tenetur itaque dolorum minus possimus nesciunt est, fugit eveniet alias natus voluptas cupiditate!",
+      price,
     });
     await camp.save();
   }
