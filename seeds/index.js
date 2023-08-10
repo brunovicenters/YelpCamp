3c6e44f9b45121568f967a346b2206851829ec49
Bruno Vicente <brunovicente.dev@gmail.com>
2023-08-10 14:12:15 -0300

    Added author property

--- seeds/index.js
+++ seeds/index.js
@@ -35,6 +35,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: "64d255b7fd885687626a660e",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: await seedImg(),
