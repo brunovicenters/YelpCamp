ef6c1cdcc144fa0cc9c13a0d8b99ca029b568ba1
Bruno Vicente <brunovicente.dev@gmail.com>
2023-08-11 15:07:52 -0300

    Reestructed routes

--- routes/users.js
+++ routes/users.js
@@ -5,21 +5,22 @@ const catchAsync = require("../utils/catchAsync");
 const { storeReturnTo } = require("../middleware");
 const users = require("../controllers/users");
 
-router.get("/register", users.renderRegister);
+router
+  .route("/register")
+  .get(users.renderRegister)
+  .post(catchAsync(users.newUser));
 
-router.post("/register", catchAsync(users.newUser));
-
-router.get("/login", users.renderLogin);
-
-router.post(
-  "/login",
-  storeReturnTo,
-  passport.authenticate("local", {
-    failureFlash: true,
-    failureRedirect: "/login",
-  }),
-  users.login
-);
+router
+  .route("/login")
+  .get(users.renderLogin)
+  .post(
+    storeReturnTo,
+    passport.authenticate("local", {
+      failureFlash: true,
+      failureRedirect: "/login",
+    }),
+    users.login
+  );
 
 router.get("/logout", users.logout);
 
